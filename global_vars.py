c0d2d788d47d44b3804a96ae5398a6460f946e43
sologuboved <sologuboved@gmail.com>
2017-12-20 13:54:02 +0300

    prepared globals

--- global_vars.py
+++ global_vars.py
@@ -7,3 +7,19 @@ FLEECY = 'fleecy'
 BLUE = 'blue'
 BLACK = 'black'
 
+
+CYCLE = (
+    (WATERBLUE, FLEECY),
+    (FLOWERED, BLUE),
+    (GRAY, BLACK),
+    (STARRED, FLEECY),
+    (WATERBLUE, BLACK),
+    (FLOWERED, BLUE),
+    (GRAY, BLACK),
+    (STARRED, FLEECY),
+    (WATERBLUE, BLACK),
+    (FLOWERED, BLUE),
+    (GRAY, FLEECY),
+    (STARRED, BLUE)
+)
+
