6c46958292a0692061acc1589388384c36196701
sologuboved <sologuboved@gmail.com>
2017-12-20 14:29:11 +0300

    added function dummies

---
+++ follow_cycle.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from global_vars import *
+
+FILENAME = 'updates.txt'
+
+
+def write_in():
+    pass
+
+
+def read_out():
+    pass
+
+
+def slew():
+    pass
+
+
+
+
